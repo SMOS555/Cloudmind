11646de76c1b518bcf02a0287a02006618201dd2
safwaan <kingsmos0510@gmail.com>
2026-08-23 12:46:36 +0530

    Fix CORS for frontend

--- backend/main.py
+++ backend/main.py
@@ -50,7 +50,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://cloudmind-frontend.onrender.com",
-        "http://localhost:5173"
+        "http://localhost:5173",
         
     ],
     allow_credentials=True,
